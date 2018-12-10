b048d1a5b7f94c16902958dfb076b3f47d7c888a
Afia Udofia <afia@awltd.com>
2018-12-10 10:39:03 +0100

    updated

---
+++ js/script.js
@@ -0,0 +1,132 @@
+var products = [
+    {   
+        id: 1,
+        name: 'Borden',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {   
+        id: 2,
+        name: 'Organic Valley',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 3,
+        name: 'Horizon',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 4,
+        name: 'Dairy land',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 5,
+        name: 'Dean Foods',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 6,
+        name: 'Hood',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 7,
+        name: 'Promised Land',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 8,
+        name: 'Stonyfield Farm',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 9,
+        name: 'Whole Foods',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    },
+    {
+        id: 10,
+        name: 'Amul',
+        sales: {
+            day1: 1000,
+            day2: 1500,
+            day3: 1400,
+            day4: 1800,
+            day5: 1400,
+            day6: 1200,
+            day7: 1400
+        }
+    }
+]
